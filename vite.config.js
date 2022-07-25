98b989f16fda8da4e9c3c6b97ab3f216368a2b19
ollyshenton <olly.shenton@lineten.com>
2022-07-25 23:13:22 +0100

    new build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://ollyshenton.github.io/health-advice-gen/dist/",
+  base: "/health-advice-gen/dist/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
